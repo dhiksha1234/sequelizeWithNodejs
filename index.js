a7f936b090feff63f895634efcadc71db3123316
dhiksha1234 <dhiksha.sivaraman@crystaldelta.com>
2023-07-05 22:39:05 +0530

    Added controllers and routers

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 
 const port =3000; 
 
-app.get("/",(req,res) =>{
-    res.send("Welcome to Home page");
-})
+const user= require('./Routes/users');
+ 
+app.use('/user',user)
+
 app.listen(port, () =>{
     console.log(`Running on port ${port}`);
 })
